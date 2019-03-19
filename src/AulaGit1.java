068c8a5bc91e7165e706934023c32d6779922ecb
petronio <petronio.a5050@gmail.com>
2019-03-19 20:09:01 -0300

    alteração1

--- src/AulaGit1.java
+++ src/AulaGit1.java
@@ -1,5 +1,9 @@
 public class AulaGit1 {
     public static void main(String[] args){
-        
+        int a=5;
+        int b=10;
+        int c=a+b;
+        System.out.println(c);
+            
     }
 }
